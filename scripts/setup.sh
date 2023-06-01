cc0dc31f34e2ef317372eb4562bd4d8a57adba49
Amit Gupta <amit183630@gmail.com>
2023-06-02 00:56:10 +0530

    code updated

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,17 +3,17 @@
 # Install Kafka
 wget https://downloads.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz
 tar -xzf kafka_2.13-3.4.0.tgz
-rm kafka_2.13-3.4.0.tgz
-mv kafka_2.13-3.4.0.tgz /usr/local/kafka
+sudo rm kafka_2.13-3.4.0.tgz
+sudo mv kafka_2.13-3.4.0 /usr/local/kafka
 
 # Install PostgreSQL
-apt-get update
-apt-get install -y postgresql
+sudo apt-get update
+#sudo apt-get install -y postgresql
 
 # Install Redis
-apt-get install -y redis-server
+sudo apt-get install -y redis-server
 
 # Start Kafka, PostgreSQL, and Redis services
-service kafka start
-service postgresql start
-service redis-server start
+sudo service kafka start
+#sudo service postgresql start
+sudo service redis-server start
